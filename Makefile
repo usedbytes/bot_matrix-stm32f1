f304fc12365d2876f2efe8e9c1bfb3a99504e95d
Brian Starkey <stark3y@gmail.com>
2017-04-11 23:05:53 +0100

    stdio: Pull in the stdio example code
    Port it to use the USB USART instead of normal USART.
    Increases the binary size to 32kB! prtinf() is huuuuuuge

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 TARGET = main
 
-SOURCES = main.c usb_cdc.c
+SOURCES = main.c usb_cdc.c stdio.c
 
 LINKER_SCRIPT=stm32f103-bl20.ld
 
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/cm3/nvic.h>
+#include <stdio.h>
 
 #include "usb_cdc.h"
 
@@ -24,8 +25,33 @@ int main(void)
 
 	while (!usb_usart_dtr());
 
+	int i, j;
+
+	printf("\r\nStandard I/O Example.\r\n");
+
+	/* Blink the LED (PC12) on the board with every transmitted byte. */
 	while (1) {
-		usb_usart_recv(buf, 1, -1);
-		usb_usart_send(buf, 1);
+		int delay = 0;
+		char local_buf[32];
+
+		gpio_toggle(GPIOC, GPIO13);	/* LED on/off */
+		do {
+			printf("Enter the delay constant for blink : ");
+			fflush(stdout);
+			fgets(local_buf, 32, stdin);
+			delay = atoi(local_buf);
+			if (delay <= 0) {
+				printf("Error: expected a delay > 0\r\n");
+			}
+		} while (delay <= 0);
+
+		printf("Blinking with a delay of %d\r\n", delay);
+		for (j = 0; j < 10; j++) {
+			gpio_toggle(GPIOC, GPIO13);
+			for (i = 0; i < delay; i++) {	/* Wait a bit. */
+				__asm__("NOP");
+			}
+		}
 	}
+	return 0;
 }
