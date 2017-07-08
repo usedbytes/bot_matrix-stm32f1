8242d5593481b4af50fd5c1177e54d57400d259d
Brian Starkey <stark3y@gmail.com>
2017-07-08 16:02:46 +0100

    main: spi loopback implementation
    All packets which are received are sent straight back.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 TARGET = main
 
-SOURCES = main.c usb_cdc.c stdio.c
+SOURCES = main.c usb_cdc.c stdio.c spi.c
 
 LINKER_SCRIPT=stm32f103-bl20.ld
 
--- main.c
+++ main.c
@@ -1,10 +1,17 @@
+#include <libopencm3/stm32/dma.h>
+#include <libopencm3/stm32/exti.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/spi.h>
 #include <libopencm3/cm3/systick.h>
 #include <libopencm3/cm3/nvic.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "usb_cdc.h"
+#include "spi.h"
+
+#define TRACE() printf("%s:%d\r\n", __func__, __LINE__)
 
 static void setup_gpio(void) {
 	RCC_APB2ENR |= RCC_APB2ENR_IOPCEN;
@@ -36,46 +43,84 @@ static void delay_ms(uint32_t ms)
 	while (msTicks < end);
 }
 
+static void panic(void)
+{
+	while (1) {
+		delay_ms(500);
+		gpio_toggle(GPIOC, GPIO13);
+	}
+}
+void hard_fault_handler(void)
+{
+	panic();
+}
+void bus_fault_handler(void)
+{
+	panic();
+}
+void usage_fault_handler(void)
+{
+	panic();
+}
+
+static void delay_us(uint32_t us)
+{
+	while (us--) {
+		int i = 72;
+		while (i--) {
+			asm("nop");
+		}
+	}
+}
+
+static inline void blink(void)
+{
+	gpio_clear(GPIOC, GPIO13);
+	delay_us(1);
+	gpio_set(GPIOC, GPIO13);
+	delay_us(2);
+}
+
+static inline void blink2(uint32_t ontime)
+{
+	gpio_clear(GPIOC, GPIO13);
+	delay_us(ontime);
+	gpio_set(GPIOC, GPIO13);
+	delay_us(2);
+}
+
 int main(void)
 {
-	char buf[16];
-
 	rcc_clock_setup_in_hse_8mhz_out_72mhz();
 
 	rcc_periph_clock_enable(RCC_GPIOC);
+	rcc_periph_clock_enable(RCC_GPIOA);
+	rcc_periph_clock_enable(RCC_AFIO);
+	rcc_periph_clock_enable(RCC_SPI1);
+	rcc_periph_clock_enable(RCC_DMA1);
 
-	setup_gpio();
 	setup_systick();
+	setup_gpio();
 
 	usb_cdc_init();
+	spi_init();
+	spi_slave_enable(SPI1);
+
+	gpio_set(GPIOC, GPIO13);
 
 	while (!usb_usart_dtr());
-
-	int i, j;
-
 	printf("\r\nStandard I/O Example.\r\n");
 
-	/* Blink the LED (PC12) on the board with every transmitted byte. */
+	spi_dump_lists();
+
+	struct spi_pl_packet *pkt;
 	while (1) {
-		int delay = 0;
-		char local_buf[32];
-
-		gpio_toggle(GPIOC, GPIO13);	/* LED on/off */
-		do {
-			printf("Enter the delay constant for blink : ");
-			fflush(stdout);
-			fgets(local_buf, 32, stdin);
-			delay = atoi(local_buf);
-			if (delay <= 0) {
-				printf("Error: expected a delay > 0\r\n");
-			}
-		} while (delay <= 0);
-
-		printf("Blinking with a delay of %d\r\n", delay);
-		for (j = 0; j < 10; j++) {
-			gpio_toggle(GPIOC, GPIO13);
-			delay_ms(delay);
+		delay_ms(10);
+		spi_dump_trace();
+		while ((pkt = spi_receive_packet())) {
+			spi_send_packet(pkt);
 		}
 	}
+
 	return 0;
 }
