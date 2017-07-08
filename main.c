03a62cfb34283991eba0c2405bd60992e21253a1
Brian Starkey <stark3y@gmail.com>
2017-07-08 16:19:02 +0100

    util: systick: Break some bits out of main.c

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/spi.h>
-#include <libopencm3/cm3/systick.h>
 #include <libopencm3/cm3/nvic.h>
 #include <stdio.h>
 #include <string.h>
@@ -11,82 +10,14 @@
 #include "usb_cdc.h"
 #include "spi.h"
 
+#include "systick.h"
+
 #define TRACE() printf("%s:%d\r\n", __func__, __LINE__)
 
 static void setup_gpio(void) {
 	RCC_APB2ENR |= RCC_APB2ENR_IOPCEN;
 	GPIOC_CRH = (GPIO_CNF_OUTPUT_PUSHPULL << (((13 - 8) * 4) + 2));
 	GPIOC_CRH |= (GPIO_MODE_OUTPUT_2_MHZ << ((13 - 8) * 4));
-}
-
-volatile uint32_t msTicks;
-
-void sys_tick_handler(void)
-{
-	msTicks++;
-}
-
-static void setup_systick(void)
-{
-	systick_set_clocksource(STK_CSR_CLKSOURCE_AHB_DIV8);
-	systick_set_reload(8999);
-	systick_interrupt_enable();
-	systick_counter_enable();
-}
-
-static void delay_ms(uint32_t ms)
-{
-	uint32_t end = msTicks + ms;
-	if (end < msTicks) {
-		while (msTicks <= 0xffffffff);
-	}
-	while (msTicks < end);
-}
-
-static void panic(void)
-{
-	while (1) {
-		delay_ms(500);
-		gpio_toggle(GPIOC, GPIO13);
-	}
-}
-void hard_fault_handler(void)
-{
-	panic();
-}
-void bus_fault_handler(void)
-{
-	panic();
-}
-void usage_fault_handler(void)
-{
-	panic();
-}
-
-static void delay_us(uint32_t us)
-{
-	while (us--) {
-		int i = 72;
-		while (i--) {
-			asm("nop");
-		}
-	}
-}
-
-static inline void blink(void)
-{
-	gpio_clear(GPIOC, GPIO13);
-	delay_us(1);
-	gpio_set(GPIOC, GPIO13);
-	delay_us(2);
-}
-
-static inline void blink2(uint32_t ontime)
-{
-	gpio_clear(GPIOC, GPIO13);
-	delay_us(ontime);
-	gpio_set(GPIOC, GPIO13);
-	delay_us(2);
 }
 
 int main(void)
@@ -99,7 +30,7 @@ int main(void)
 	rcc_periph_clock_enable(RCC_SPI1);
 	rcc_periph_clock_enable(RCC_DMA1);
 
-	setup_systick();
+	systick_init();
 	setup_gpio();
 
 	usb_cdc_init();
