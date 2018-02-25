d713a321a47251a6f725fc90406b35a59cb01f19
Brian Starkey <stark3y@gmail.com>
2018-02-25 15:06:27 +0000

    main: use the new motor code

--- main.c
+++ main.c
@@ -11,37 +11,14 @@
 #include <stdarg.h>
 
 #include "hbridge.h"
+#include "motor.h"
 #include "pwm.h"
-#include "usb_cdc.h"
 #include "spi.h"
-#include "period_counter.h"
-#include "controller.h"
+#include "usb_cdc.h"
 
 #include "systick.h"
 
 #define TRACE() printf("%s:%d\r\n", __func__, __LINE__)
-
-const struct gain gains[] = {
-	{ FP_VAL(-1), 0, 0 },
-	{ FP_VAL(-5), 0, 0 },
-	{ FP_VAL(-10), 0, 0 },
-	{ FP_VAL(-25), 0, 0 },
-	{ FP_VAL(-100), 0, 0 },
-	{ FP_VAL(-130), 0, 0 },
-	{ FP_VAL(-200), 0, 0 },
-};
-
-const uint16_t gs_limits[] = {
-	4000,
-	3500,
-	6000,
-	10000,
-	18000,
-	35000,
-	65535,
-};
-
-volatile uint16_t duty = 0x4000;
 
 static void setup_irq_priorities(void)
 {
@@ -74,22 +51,6 @@ static void setup_irq_priorities(void)
 
 extern char dbg[256];
 extern volatile bool cp;
-
-struct hbridge hb = {
-	.timer = TIM2,
-	.a = {
-		.ch1 = TIM_OC1,
-		.ch2 = TIM_OC2,
-	},
-	.b = {
-		.ch1 = TIM_OC3,
-		.ch2 = TIM_OC4,
-	},
-};
-
-struct controller controller = {
-
-};
 
 static void setup_gpio(void) {
 	RCC_APB2ENR |= RCC_APB2ENR_IOPCEN;
@@ -124,125 +85,6 @@ static void ep0_process_packet(struct spi_pl_packet *pkt)
 	}
 }
 
-static void ep2_process_packet(struct spi_pl_packet *pkt)
-{
-	if ((pkt->type != 2) || (pkt->flags & SPI_FLAG_ERROR))
-		return;
-
-	uint32_t freq = *((uint32_t*)pkt->data);
-
-	hbridge_set_freq(&hb, freq);
-}
-
-static void ep3_process_packet(struct spi_pl_packet *pkt)
-{
-	if ((pkt->type != 3) || (pkt->flags & SPI_FLAG_ERROR))
-		return;
-
-	uint16_t lduty = *((uint16_t*)(pkt->data + 2));
-
-	printf("Set duty: %x\r\n", lduty);
-	duty = lduty;
-
-	hbridge_set_duty(&hb, pkt->data[0], pkt->data[1], lduty);
-}
-
-struct gain_set {
-	int32_t Kc, Kd, Ki;
-};
-
-static void ep4_process_packet(struct spi_pl_packet *pkt)
-{
-	struct gain_set *gs;
-	if ((pkt->type != 4) || (pkt->flags & SPI_FLAG_ERROR))
-		return;
-
-	gs = (struct gain_set *)pkt->data;
-
-	printf("Gains: %lx %lx %lx\r\n", gs->Kc, gs->Kd, gs->Ki);
-
-	controller_set_gains(&controller, gs->Kc, gs->Kd, gs->Ki);
-}
-
-static void ep5_process_packet(struct spi_pl_packet *pkt)
-{
-	if ((pkt->type != 5) || (pkt->flags & SPI_FLAG_ERROR))
-		return;
-
-	uint32_t set_point = *((uint32_t*)(pkt->data));
-
-	printf("Setpoint: %lu\r\n", set_point);
-
-	controller_set(&controller, set_point);
-}
-
-static void ep6_process_packet(struct spi_pl_packet *pkt)
-{
-	if ((pkt->type != 6) || (pkt->flags & SPI_FLAG_ERROR))
-		return;
-
-	int32_t ilimit = *((int32_t*)(pkt->data));
-
-	printf("ilimit: %li\r\n", ilimit);
-
-	controller_set_ilimit(&controller, ilimit);
-}
-
-struct period_counter pc = {
-	.timer = TIM4,
-};
-
-void tim4_isr(void)
-{
-	period_counter_update(&pc);
-}
-
-void tim3_isr(void)
-{
-	int32_t delta = 0;
-	uint8_t gs_idx;
-	uint32_t count = period_counter_get(&pc, PC_CH1);
-
-	for (gs_idx = 0; gs_idx < sizeof(gains) / sizeof(gains[0]); gs_idx++) {
-		if (duty < gs_limits[gs_idx])
-			break;
-	}
-
-	timer_clear_flag(TIM3, TIM_SR_UIF);
-	delta = controller_tick(&controller, count, gs_idx);
-	if (!delta)
-		return;
-
-	if (delta > 0 && ((0x10000 - delta) < duty)) {
-		duty = 0xffff;
-	} else if ((delta < 0) && (-delta > duty)) {
-		duty = 0;
-	} else {
-		duty += delta;
-	}
-
-	hbridge_set_duty(&hb, HBRIDGE_A, false, duty);
-
-}
-
-static void pid_timer_init(uint32_t timer)
-{
-	timer_reset(timer);
-	timer_slave_set_mode(timer, TIM_SMCR_SMS_OFF);
-	timer_set_prescaler(timer, 7100);
-	timer_set_mode(timer, TIM_CR1_CKD_CK_INT,
-		       TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
-	timer_enable_preload(timer);
-	timer_update_on_overflow(timer);
-	timer_enable_update_event(timer);
-	timer_generate_event(timer, TIM_EGR_UG);
-
-	timer_enable_irq(timer, TIM_DIER_UIE);
-	nvic_enable_irq(NVIC_TIM3_IRQ);
-	timer_set_period(timer, 500);
-	timer_enable_counter(timer);
-}
-
 static void ep0xfe_process_packet(struct spi_pl_packet *pkt)
 {
 	if ((pkt->type != 0xfe) || (pkt->flags & SPI_FLAG_ERROR))
@@ -252,6 +94,8 @@ static void ep0xfe_process_packet(struct spi_pl_packet *pkt)
 
 	scb_reset_system();
 }
+
+#define EP_MOTORS 18
 
 #define MESSAGE_PKT_TYPE 0xff
 #define MESSAGE_LEVEL_INFO 10
@@ -342,17 +186,8 @@ int main(void)
 	while (!usb_usart_dtr());
 	printf("\r\nStandard I/O Example.\r\n");
 
-	hbridge_init(&hb);
-	hbridge_set_duty(&hb, HBRIDGE_A, false, 0x2000);
-
-	controller_init(&controller, gains, sizeof(gains) / sizeof(gains[0]));
-	controller_set(&controller, 1000);
-	//controller_set_gains(&controller, -0x10000, 0, 0);
-
-	period_counter_init(&pc);
-	period_counter_enable(&pc, PC_CH1);
-	pid_timer_init(TIM3);
-	spi_dump_lists();
+	motor_init();
+	motor_enable_loop();
 
 	setup_irq_priorities();
 
@@ -376,6 +211,11 @@ int main(void)
 					ep1_process_packet(pkt);
 					spi_free_packet(pkt);
 					break;
+				case EP_MOTORS:
+					motor_process_packet(pkt);
+					spi_free_packet(pkt);
+					break;
+				/*
 				case 2:
 					ep2_process_packet(pkt);
 					spi_free_packet(pkt);
@@ -396,6 +236,7 @@ int main(void)
 					ep6_process_packet(pkt);
 					spi_free_packet(pkt);
 					break;
+				*/
 				case 0xfe:
 					ep0xfe_process_packet(pkt);
 					spi_free_packet(pkt);
@@ -409,8 +250,6 @@ int main(void)
 		if (cp) {
 			printf("%s", dbg);
 			cp = false;
-			printf("Duty: %u\r\n", duty);
-			printf("Count: %lu\r\n", period_counter_get(&pc, PC_CH1));
 		}
 
 		if (msTicks - time >= 100) {
